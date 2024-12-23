da9abea816a8f347ee3b7f1496b1b81c2803ee28
João Anderson <joao.anderson.af@gmail.com>
2024-12-23 15:44:46 -0300

    Configurando classe `ScheduleType` para figurar como tabela do banco de dados e ajustando o MER

--- api/src/main/java/apisemaperreio/escalante/model/ScheduleType.java
+++ api/src/main/java/apisemaperreio/escalante/model/ScheduleType.java
@@ -1,13 +1,28 @@
 package apisemaperreio.escalante.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class ScheduleType {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false, length = 50)
     private String name;
+    @Column(nullable = false)
     private Integer daysWorked;
+    @Column(nullable = false)
     private Integer daysOff;
+    @Column(nullable = false, columnDefinition = "INT DEFAULT 1")
     private Integer multiplier;
+    @Column(length = 255)
+    private String description;
 
 }
