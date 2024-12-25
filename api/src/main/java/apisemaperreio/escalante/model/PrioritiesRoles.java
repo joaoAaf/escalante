46c7b1eeaf554a5e3083c432e98f5cb0950e445b
João Anderson <joao.anderson.af@gmail.com>
2024-12-25 15:47:53 -0300

    Criando classe e tabela `PrioritiesRoles` e Refatorando

---
+++ api/src/main/java/apisemaperreio/escalante/model/PrioritiesRoles.java
@@ -0,0 +1,12 @@
+package apisemaperreio.escalante.model;
+
+import lombok.Data;
+
+@Data
+public class PrioritiesRoles {
+
+    private Role role;
+    private WorkerPosition position;
+    private Short priority;
+
+}
--- api/src/main/java/apisemaperreio/escalante/model/Worker.java
+++ api/src/main/java/apisemaperreio/escalante/model/Worker.java
@@ -10,14 +10,14 @@ public class Worker {
     private String registration;
     private String name;
     private Character sex;
-    private WorkerPosition position;
-    private Integer rank;
+    private Short rank;
     private String phone;
     private String email;
     private LocalDate birthdate;
     private LocalDate lastDayWorked;
     private Boolean driver;
     private Boolean scheduleable;
+    private WorkerPosition position;
     private ScheduleType scheduleType;
 
 }
