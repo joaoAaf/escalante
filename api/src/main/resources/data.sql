a830143345dab76751548a4119fd2107a229a996
João Anderson <joao.anderson.af@gmail.com>
2024-12-25 18:47:01 -0300

    criando carga inicial para tabela `worker_role`

--- api/src/main/resources/data.sql
+++ api/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 -- Carga inicial tabela shedule_type
 INSERT INTO schedule_type (name, days_worked, days_off, multiplier) VALUES
-    ('Escala 1/3', 1, 3, 2),
-    ('Escala 1/4', 1, 4, 2);
+    ('escala 1/3', 1, 3, 2),
+    ('escala 1/4', 1, 4, 2);
 
 -- Carga inicial tabela position
 INSERT INTO worker_position (name) VALUES
@@ -9,3 +9,11 @@ INSERT INTO worker_position (name) VALUES
     ('sargento'),
     ('cabo'),
     ('soldado');
+
+--  Carga inicial tabela worker_role
+INSERT INTO worker_role (name, id_schedule_type) VALUES
+    ('fiscal', 2),
+    ('chefe de linha', 1),
+    ('auxiliar de linha', 1),
+    ('permanente', 1),
+    ('motorista', 2);
