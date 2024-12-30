2c0ebabda21bb01bad368b0cce8cf0a1121fa157
João Anderson <joao.anderson.af@gmail.com>
2024-12-30 17:04:42 -0300

    Adicionando consultas ao `WorkerRepository` para filtrar pelos atributos `driver` e `position`, além de ajustar a tabela `schedule_type`

--- api/src/main/java/apisemaperreio/escalante/repository/WorkerRepository.java
+++ api/src/main/java/apisemaperreio/escalante/repository/WorkerRepository.java
@@ -1,11 +1,29 @@
 package apisemaperreio.escalante.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import apisemaperreio.escalante.model.Worker;
 
 @Repository
 public interface WorkerRepository extends JpaRepository<Worker, Integer> {
+
+    // Consulta que retorna todos os trabalhadores que são motoristas ordenados
+    // pela data do último dia trabalhado e senioridade em ordem crescente
+    List<Worker> findByDriverOrderByLastDayWorkedAscSeniorityAsc(Boolean driver);
+    
+    // Consulta que retorna todos os trabalhadores ordenados pela data do último dia trabalhado em ordem crescente
+    // e pela senioridade em ordem decrescente
+    @Query("SELECT w FROM Worker w JOIN w.position p WHERE p.name = :positionName ORDER BY w.lastDayWorked ASC, w.seniority DESC")
+    List<Worker> findByPositionNameSeniorityDesc(@Param("positionName") String positionName);
+
+    // Consulta que retorna todos os trabalhadores ordenados pela data do último dia trabalhado em ordem crescente
+    // e pela senioridade em ordem crescente
+    @Query("SELECT w FROM Worker w JOIN w.position p WHERE p.name = :positionName ORDER BY w.lastDayWorked ASC, w.seniority ASC")
+    List<Worker> findByPositionNameSeniorityAsc(@Param("positionName") String positionName);
 
 }
--- api/src/main/resources/data.sql
+++ api/src/main/resources/data.sql
@@ -2,7 +2,7 @@
 INSERT INTO schedule_type (name, days_worked, days_off, multiplier) VALUES
     ('escala 1/3', 1, 3, 2),
     ('escala 1/4', 1, 4, 2),
-    ('escala 1/7', 1, 7, 1);
+    ('escala 1/7', 1, 7, 2);
 
 -- Carga inicial tabela position
 INSERT INTO worker_position (name) VALUES
