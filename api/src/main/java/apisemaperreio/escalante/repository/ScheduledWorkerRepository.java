011ecfa7553deba47aea4f28b649c1c42eee14f9
João Anderson <joao.anderson.af@gmail.com>
2025-01-03 19:10:19 -0300

    Criando consulta para verificar trabalhadores na tabela `scheduled_worker`

--- api/src/main/java/apisemaperreio/escalante/repository/ScheduledWorkerRepository.java
+++ api/src/main/java/apisemaperreio/escalante/repository/ScheduledWorkerRepository.java
@@ -1,5 +1,7 @@
 package apisemaperreio.escalante.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import apisemaperreio.escalante.model.ScheduledWorker;
 
 @Repository
 public interface ScheduledWorkerRepository extends JpaRepository<ScheduledWorker, Integer> {
+
+    Optional<ScheduledWorker> findFirstByWorkerIdOrderByDateDesc(Integer workerId);
 
 }
