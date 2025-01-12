27ecd88044cbb36edb7900b7346f80b4f67fcea4
João Anderson <joao.anderson.af@gmail.com>
2025-01-12 19:56:11 -0300

    Refatorando e criando algumas validações para o `ScheduledWorkerController`

--- api/src/main/java/apisemaperreio/escalante/controller/ScheduledWorkerController.java
+++ api/src/main/java/apisemaperreio/escalante/controller/ScheduledWorkerController.java
@@ -1,16 +1,21 @@
 package apisemaperreio.escalante.controller;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import apisemaperreio.escalante.service.ScheduledWorkerService;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
-
 
 @RequiredArgsConstructor
 @RequestMapping("/schedule")
@@ -18,30 +23,49 @@ import lombok.RequiredArgsConstructor;
 public class ScheduledWorkerController {
 
     private final ScheduledWorkerService service;
-    
-    // Para Testes (Excluir depois)
-    LocalDate date = LocalDate.parse("2024-09-01");
 
-    @GetMapping
-    public ResponseEntity<Object> getAllScheduledWorkers() {
+    @GetMapping("{date}")
+    public ResponseEntity<Object> getScheduledWorkerByDate(@PathVariable String date) {
         try {
-            var scheduledWorkers = service.getAllScheduledWorkers();
+            var scheduledWorkers = service.getScheduledWorkerByDate(LocalDate.parse(date));
             if (scheduledWorkers.isEmpty()) {
                 return new ResponseEntity<>("No scheduled workers found", HttpStatus.NOT_FOUND);
             }
             return new ResponseEntity<>(scheduledWorkers, HttpStatus.OK);
+        } catch (DateTimeParseException e) {
+            return new ResponseEntity<>("Invalid date format: yyyy-MM-dd", HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>("Service unavailable", HttpStatus.SERVICE_UNAVAILABLE);
         }
     }
 
-    @GetMapping("/test")
-    public ResponseEntity<Object> getTest() {
+    @GetMapping
+    public ResponseEntity<Object> getRangeScheduledWorkers(@RequestParam(value = "start") String startDate,
+            @RequestParam(value = "end") String endDate) {
         try {
-            var scheduledWorkers = service.scheduler(date, LocalDate.parse("2024-09-30"), 2);
+            var scheduledWorkers = service.getRangeScheduledWorkers(LocalDate.parse(startDate),
+                    LocalDate.parse(endDate));
+            if (scheduledWorkers.isEmpty()) {
+                return new ResponseEntity<>("No scheduled workers found", HttpStatus.NOT_FOUND);
+            }
             return new ResponseEntity<>(scheduledWorkers, HttpStatus.OK);
+        } catch (DateTimeParseException e) {
+            return new ResponseEntity<>("Invalid date format: yyyy-MM-dd", HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>("Service unavailable", HttpStatus.SERVICE_UNAVAILABLE);
+        }
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<Object> scheduler(@RequestParam(value = "start") String startDate,
+            @RequestParam(value = "end") String endDate, @RequestParam(value = "days") @Max(3) @Positive Integer days) {
+        try {
+            var scheduledWorkers = service.scheduler(LocalDate.parse(startDate), LocalDate.parse(endDate), days);
+            return new ResponseEntity<>(scheduledWorkers, HttpStatus.CREATED);
+        } catch (DateTimeParseException e) {
+            return new ResponseEntity<>("Invalid date format: yyyy-MM-dd", HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Service unavailable", HttpStatus.SERVICE_UNAVAILABLE);
         }
     }
 
