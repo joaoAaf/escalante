ac5a098f4a4e590ae7926b245e1b1378ebb06282
João Anderson <joao.anderson.af@gmail.com>
2025-11-24 09:24:38 -0300

    Adicionando suporte para serviços anteriores na criação da escala automática

--- apis/src/main/java/apisemaperreio/escalante/escalante/dtos/DadosEscalaRequest.java
+++ apis/src/main/java/apisemaperreio/escalante/escalante/dtos/DadosEscalaRequest.java
@@ -3,5 +3,5 @@ package apisemaperreio.escalante.escalante.dtos;
 import java.time.LocalDate;
 import java.util.List;
 
-public record DadosEscalaRequest(LocalDate dataInicio, LocalDate dataFim, int diasServico, List<MilitarEscalavel> militares) {
+public record DadosEscalaRequest(LocalDate dataInicio, LocalDate dataFim, int diasServico, List<MilitarEscalavel> militares, List<ServicoOperacionalDto> servicosAnteriores) {
 }
--- apis/src/main/java/apisemaperreio/escalante/escalante/services/EscalaServiceEscalante.java
+++ apis/src/main/java/apisemaperreio/escalante/escalante/services/EscalaServiceEscalante.java
@@ -2,19 +2,24 @@ package apisemaperreio.escalante.escalante.services;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import apisemaperreio.escalante.escalante.domain.DadosEscala;
+import apisemaperreio.escalante.escalante.domain.Funcao;
+import apisemaperreio.escalante.escalante.domain.ServicoOperacional;
 import apisemaperreio.escalante.escalante.dtos.DadosEscalaRequest;
 import apisemaperreio.escalante.escalante.dtos.ServicoOperacionalDto;
 import apisemaperreio.escalante.escalante.usecases.EscalaUseCasesEscalante;
 import apisemaperreio.escalante.escalante.utils.adapters.exportador_xlsx.ExportadorXLSXAdapter;
 import apisemaperreio.escalante.escalante.utils.adapters.importador_xlsx.ImportadorEscalaXLSXAdapter;
 import apisemaperreio.escalante.escalante.utils.factories.EscalaFactory;
+import apisemaperreio.escalante.escalante.utils.factories.ServicoOperacionalFactory;
 import apisemaperreio.escalante.escalante.utils.mappers.MilitarMapperEscalante;
 import apisemaperreio.escalante.escalante.utils.mappers.ServicoOperacionalMapper;
 
@@ -51,9 +56,23 @@ public class EscalaServiceEscalante implements EscalaUseCasesEscalante {
 
     @Override
     public List<ServicoOperacionalDto> criarEscalaAutomatica(DadosEscalaRequest request) {
+        var militares = militarMapper.toListMilitar(request.militares());
+        var servicosAnteriores = new ArrayList<ServicoOperacional>();
+        if (request.servicosAnteriores() != null && !request.servicosAnteriores().isEmpty()) {
+            request.servicosAnteriores().forEach(servicoDto -> {
+                var servico = ServicoOperacionalFactory.criarServicoOperacional(Funcao.obterFuncaoPorNome(servicoDto.funcao()), servicoDto.dataServico());
+                var militar = militares.stream()
+                        .filter(m -> m.getMatricula().equals(servicoDto.matricula()))
+                        .findFirst()
+                        .orElseThrow(() -> new NoSuchElementException("Militar não encontrado: " + servicoDto.matricula()));
+                servico.setMilitar(militar);
+                servicosAnteriores.add(servico);
+            });
+            militares.forEach(militar -> militar.ultimosServicosConsecutivos(servicosAnteriores));
+        }
         var dadosEscala = new DadosEscala(request.dataInicio(), request.dataFim(), request.diasServico());
         var escala = EscalaFactory.criarEscala(dadosEscala);
-        escala.preencherEscala(militarMapper.toListMilitar(request.militares()));
+        escala.preencherEscala(militares);
         return servicoOperacionalMapper.toListServicoOperacionalDto(escala.getServicosEscala());
     }
 
