ea71d83970d357e7f871183124e9d3cc714e7a82
João Anderson <joao.anderson.af@gmail.com>
2025-11-28 15:36:30 -0300

    Atualizando idioma do HTML para pt-BR, versão do pacote para 0.0.2 e corrigindo origem do servidor para localhost

--- front/vite.config.js
+++ front/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 8000,
     strictPort: true,
     host: true,
-    origin: "http://0.0.0.0:8000",
+    origin: "http://localhost:8000",
     proxy: {
       '/api': {
         target: "http://localhost:8080",
