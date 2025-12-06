4f7198f0c1a05f42e4271b540d2f0275572bcff0
João Anderson <joao.anderson.af@gmail.com>
2025-12-06 12:35:22 -0300

    Adicionando a classe Role e implementando gerenciamento de papéis para Admin. Atualizando AdminAutenticado para retornar autoridades baseadas nos papéis do administrador.

--- apis/src/main/java/apisemaperreio/escalante/escalante/domain/AdminAutenticado.java
+++ apis/src/main/java/apisemaperreio/escalante/escalante/domain/AdminAutenticado.java
@@ -1,9 +1,10 @@
 package apisemaperreio.escalante.escalante.domain;
 
 import java.util.Collection;
-import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public class AdminAutenticado implements UserDetails {
@@ -16,7 +17,9 @@ public class AdminAutenticado implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(() -> "ROLE_ADMIN");
+    return admin.getRoles().stream()
+        .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))
+        .collect(Collectors.toList());
     }
 
     @Override
---
+++ apis/src/main/java/apisemaperreio/escalante/escalante/domain/Role.java
@@ -0,0 +1,5 @@
+package apisemaperreio.escalante.escalante.domain;
+
+public enum Role {
+    ADMIN
+}
