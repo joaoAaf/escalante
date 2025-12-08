cc087e69bebb64c6d27ce71009e8b8aba33e2400
João Anderson <joao.anderson.af@gmail.com>
2025-12-08 15:51:32 -0300

    Adicionando suporte para autenticação baseada em perfil no LoginController e LoginService. Um usuário só poderá logar utilizando um único perfil.

--- apis/src/main/java/br/com/appsemaperreio/escalante_api/controllers/seguranca/LoginController.java
+++ apis/src/main/java/br/com/appsemaperreio/escalante_api/controllers/seguranca/LoginController.java
@@ -6,8 +6,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.appsemaperreio.escalante_api.domain.seguranca.Perfil;
 import br.com.appsemaperreio.escalante_api.services.seguranca.LoginService;
 
 @RestController
@@ -21,8 +23,14 @@ public class LoginController {
     }
 
     @PostMapping
-    public ResponseEntity<Map<String, String>>login(Authentication authentication) {
-        return ResponseEntity.ok(Map.of("bearerToken", loginService.login(authentication)));
+    public ResponseEntity<Map<String, String>> login(Authentication authentication, @RequestParam String perfil) {
+        try {
+            var perfilEnum = Perfil.valueOf(perfil.toUpperCase());
+            return ResponseEntity
+                    .ok(Map.of("bearerToken", loginService.login(authentication, perfilEnum)));
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Perfil inválido: " + perfil);
+        }
     }
 
 }
