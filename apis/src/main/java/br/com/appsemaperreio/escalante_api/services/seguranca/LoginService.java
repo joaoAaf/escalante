f2fbc8a19a5d50f99d781d8bf33784ae69794292
João Anderson <joao.anderson.af@gmail.com>
2025-12-08 15:51:32 -0300

    Adicionando suporte para autenticação baseada em perfil no LoginController e LoginService. Um usuário só poderá logar utilizando um único perfil.

--- apis/src/main/java/br/com/appsemaperreio/escalante_api/services/seguranca/LoginService.java
+++ apis/src/main/java/br/com/appsemaperreio/escalante_api/services/seguranca/LoginService.java
@@ -1,7 +1,14 @@
 package br.com.appsemaperreio.escalante_api.services.seguranca;
 
+import java.util.Collections;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
+
+import br.com.appsemaperreio.escalante_api.domain.seguranca.Perfil;
 
 @Service
 public class LoginService {
@@ -12,7 +19,17 @@ public class LoginService {
         this.jwtService = jwtService;
     }
 
-    public String login(Authentication authentication) {
-        return jwtService.gerarToken(authentication);
+    public String login(Authentication authentication, Perfil perfil) {
+        var authority = authentication.getAuthorities().stream()
+                .filter(auth -> auth.getAuthority().equals("ROLE_" + perfil.name()))
+                .findFirst()
+                .orElseThrow(() -> new AccessDeniedException("Acesso negado para o perfil: " + perfil.name()));
+
+        var newAuthentication = new UsernamePasswordAuthenticationToken(
+                authentication.getPrincipal(),
+                null,
+                Collections.singletonList(
+                        new SimpleGrantedAuthority(authority.getAuthority())));
+        return jwtService.gerarToken(newAuthentication);
     }
 }
