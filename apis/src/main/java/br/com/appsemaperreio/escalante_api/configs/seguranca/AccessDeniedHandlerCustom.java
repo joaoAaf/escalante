943091827c3f846b0beb3bb9e8d38b43a096e386
João Anderson <joao.anderson.af@gmail.com>
2025-12-11 19:45:27 -0300

    Padronizando as mensagens de tratamento de erro de acesso e autenticação para respostas não privilegiadas e adicionando logs detalhando erros.

--- apis/src/main/java/br/com/appsemaperreio/escalante_api/configs/seguranca/AccessDeniedHandlerCustom.java
+++ apis/src/main/java/br/com/appsemaperreio/escalante_api/configs/seguranca/AccessDeniedHandlerCustom.java
@@ -6,6 +6,8 @@ import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
@@ -21,19 +23,19 @@ import jakarta.servlet.http.HttpServletResponse;
 public class AccessDeniedHandlerCustom implements AccessDeniedHandler {
 
     private static final DateTimeFormatter FORMATO_DATA = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static final Logger LOG = LoggerFactory.getLogger(AccessDeniedHandlerCustom.class);
     private final ObjectMapper mapper = new ObjectMapper();
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response,
             AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        
+        LOG.debug("AccessDeniedException: {}", request.getRequestURI(), accessDeniedException.getMessage(), accessDeniedException);
 
         Map<String, String> body = new HashMap<>();
         body.put("Data/Hora", LocalDateTime.now().format(FORMATO_DATA));
         body.put("Status", HttpStatus.FORBIDDEN.toString());
-        String msg = accessDeniedException != null && accessDeniedException.getMessage() != null
-                ? accessDeniedException.getMessage()
-                : "Acesso proibido";
-        body.put("Mensagem", msg);
+        body.put("Mensagem", "Acesso negado");
 
         response.setStatus(HttpStatus.FORBIDDEN.value());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
