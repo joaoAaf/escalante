ff6011b0422c63d17679446f62cd137d5b421e6b
João Anderson <joao.anderson.af@gmail.com>
2025-12-14 15:58:08 -0300

    Refatorando LoginService para retornar um token vazio caso o password seja o padrão ou o inicial, além de delegar a validação de perfil para o PerfilMapper.

--- apis/src/main/java/br/com/appsemaperreio/escalante_api/application/seguranca/service/LoginService.java
+++ apis/src/main/java/br/com/appsemaperreio/escalante_api/application/seguranca/service/LoginService.java
@@ -1,33 +1,56 @@
 package br.com.appsemaperreio.escalante_api.application.seguranca.service;
 
 import java.util.Collections;
+import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.appsemaperreio.escalante_api.application.seguranca.LoginUseCases;
-import br.com.appsemaperreio.escalante_api.domain.seguranca.Perfil;
+import br.com.appsemaperreio.escalante_api.application.seguranca.mappers.PerfilMapper;
 
 @Service
 public class LoginService implements LoginUseCases {
 
     private final JwtService jwtService;
+    private final PerfilMapper perfilMapper;
+    private final PasswordEncoder passwordEncoder;
+    private final String senhaInicial;
+    private final String senhaPadrao;
 
-    public LoginService(JwtService jwtService) {
+    public LoginService(
+            JwtService jwtService,
+            PerfilMapper perfilMapper,
+            PasswordEncoder passwordEncoder,
+            @Value("${env.usuario.inicial.password}") String senhaInicial,
+            @Value("${env.usuario.padrao.password}") String senhaPadrao) {
         this.jwtService = jwtService;
+        this.perfilMapper = perfilMapper;
+        this.passwordEncoder = passwordEncoder;
+        this.senhaInicial = senhaInicial;
+        this.senhaPadrao = senhaPadrao;
     }
 
     @Override
     public String login(Authentication authentication, String perfilString) {
-        Perfil perfil;
-        try {
-            perfil = Perfil.valueOf(perfilString.toUpperCase());
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("Perfil inválido: " + perfilString);
+        
+        var principal = (UserDetails) authentication.getPrincipal();
+
+        var passwordsPadroes = Set.of(senhaInicial, senhaPadrao);
+
+        for (String pass : passwordsPadroes) {
+            var invalido = pass == null || pass.isBlank();    
+            if (!invalido && passwordEncoder.matches(pass, principal.getPassword()))
+                return "";
         }
+        
+        var perfil = perfilMapper.stringToPerfil(perfilString);
 
         var authority = authentication.getAuthorities().stream()
                 .filter(auth -> auth.getAuthority().equals("ROLE_" + perfil.name()))
@@ -39,7 +62,7 @@ public class LoginService implements LoginUseCases {
                 null,
                 Collections.singletonList(
                         new SimpleGrantedAuthority(authority.getAuthority())));
-        
+
         return jwtService.gerarToken(newAuthentication);
     }
 }
