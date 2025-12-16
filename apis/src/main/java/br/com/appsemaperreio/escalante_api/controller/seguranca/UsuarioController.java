ce9b9e6687b6d0a6285215678ee874d5e981e0bf
João Anderson <joao.anderson.af@gmail.com>
2025-12-16 19:02:15 -0300

    Adicionando métodos para obter, listar, atualizar e deletar usuários na classe UsuarioController, além de implementar a adição e remoção de perfis.

--- apis/src/main/java/br/com/appsemaperreio/escalante_api/controller/seguranca/UsuarioController.java
+++ apis/src/main/java/br/com/appsemaperreio/escalante_api/controller/seguranca/UsuarioController.java
@@ -1,12 +1,16 @@
 package br.com.appsemaperreio.escalante_api.controller.seguranca;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.appsemaperreio.escalante_api.application.seguranca.UsuarioUseCases;
+import br.com.appsemaperreio.escalante_api.domain.seguranca.Perfil;
 import br.com.appsemaperreio.escalante_api.dto.seguranca.UsuarioRequest;
 import br.com.appsemaperreio.escalante_api.dto.seguranca.UsuarioResponse;
 import jakarta.validation.Valid;
@@ -40,11 +45,72 @@ public class UsuarioController {
         return ResponseEntity.created(URI.create("/api/usuarios/" + usuarioResponse.username())).body(usuarioResponse);
     }
 
+    @GetMapping("/{username}")
+    public ResponseEntity<UsuarioResponse> obterUsuarioPorUsername(
+            @PathVariable @NotBlank(message = "O username é obrigatório") String username) {
+        var usuarioResponse = usuarioUseCases.obterUsuarioPorUsername(username);
+        return ResponseEntity.ok(usuarioResponse);
+    }
+
+    @GetMapping("/atual")
+    public ResponseEntity<UsuarioResponse> obterUsuarioPorUsername(Authentication authentication) {
+        var username = authentication.getName();
+        var usuarioResponse = usuarioUseCases.obterUsuarioPorUsername(username);
+        return ResponseEntity.ok(usuarioResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioResponse>> listarUsuarios() {
+        var usuarios = usuarioUseCases.listarUsuarios();
+        if (usuarios.isEmpty())
+            return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(usuarios);
+    }
+
     @PatchMapping("/username")
     public ResponseEntity<Map<String, String>> atualizarUsername(Authentication authentication,
-            @RequestParam("novo") @NotBlank(message = "O nome de usuário é obrigatório") @Size(min = 3, max = 20, message = "O nome de usuário deve ter entre 3 e 20 caracteres") @Pattern(regexp = "^(?!.*[\\s\\p{Zs}])[A-Za-z0-9_.-]+$", message = "O nome de usuário deve conter apenas letras ASCII, números, '.', '_' e '-', sem espaços") String usernameNovo) {
-        var usernameAtualizado = usuarioUseCases.atualizarUsername(authentication, usernameNovo);
+            @RequestParam("novo")
+            @NotBlank(message = "O nome de usuário é obrigatório")
+            @Size(min = 3, max = 20, message = "O nome de usuário deve ter entre 3 e 20 caracteres")
+            @Pattern(regexp = "^(?!.*[\\s\\p{Zs}])[A-Za-z0-9_.-]+$",
+                message = "O nome de usuário deve conter apenas letras ASCII, números, '.', '_' e '-', sem espaços")
+            String usernameNovo) {
+        var usernameAtual = authentication.getName();
+        var usernameAtualizado = usuarioUseCases.atualizarUsername(usernameAtual, usernameNovo);
         return ResponseEntity.ok(Map.of("username", usernameAtualizado));
+    }
+
+    @PatchMapping("/password")
+    public ResponseEntity<Void> atualizarPassword(Authentication authentication,
+            @RequestParam("novo")
+            @NotBlank(message = "A nova senha é obrigatória")
+            @Size(min = 9, max = 50, message = "A nova senha deve ter entre 9 e 50 caracteres")
+            @Pattern(regexp = "^(?=.*\\p{Ll})(?=.*\\p{Lu})(?=.*\\d)(?=.*\\p{Punct})[\\p{L}\\d\\p{Punct}]+$",
+                message = "A nova senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial")
+            String passwordNovo) {
+        var username = authentication.getName();
+        usuarioUseCases.atualizarPassword(username, passwordNovo);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/perfis/adicionar")
+    public ResponseEntity<List<Perfil>> adicionarPerfis(
+            @RequestBody @Valid @NotNull(message = "O corpo da requisição é obrigatório") UsuarioRequest usuarioRequest) {
+        var perfisAdicionados = usuarioUseCases.adicionarPerfis(usuarioRequest);
+        return ResponseEntity.ok(perfisAdicionados);
+    }
+
+    @PatchMapping("/perfis/remover")
+    public ResponseEntity<List<Perfil>> removerPerfis(
+            @RequestBody @Valid @NotNull(message = "O corpo da requisição é obrigatório") UsuarioRequest usuarioRequest) {
+        var perfisRemovidos = usuarioUseCases.removerPerfis(usuarioRequest);
+        return ResponseEntity.ok(perfisRemovidos);
+    }
+
+    @DeleteMapping("/{username}")
+    public ResponseEntity<Void> deletarUsuario(@PathVariable @NotBlank(message = "O username é obrigatório") String username) {
+        usuarioUseCases.deletarUsuario(username);
+        return ResponseEntity.noContent().build();
     }
 
 }
