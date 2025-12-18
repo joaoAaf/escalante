b1618b046b2e062d2d51aab750f2cb5f011888f1
João Anderson <joao.anderson.af@gmail.com>
2025-12-18 19:24:45 -0300

    Alterando método de login para aceitar parâmetro booleano de admin e ajustando a lógica de autenticação no LoginService.

--- apis/src/main/java/br/com/appsemaperreio/escalante_api/seguranca/model/application/service/LoginService.java
+++ apis/src/main/java/br/com/appsemaperreio/escalante_api/seguranca/model/application/service/LoginService.java
@@ -1,8 +1,8 @@
 package br.com.appsemaperreio.escalante_api.seguranca.model.application.service;
 
-import java.util.Collections;
-import java.util.Set;
-
+import br.com.appsemaperreio.escalante_api.seguranca.model.application.ILoginService;
+import br.com.appsemaperreio.escalante_api.seguranca.model.application.mappers.PerfilMapper;
+import br.com.appsemaperreio.escalante_api.seguranca.model.domain.Perfil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -12,8 +12,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import br.com.appsemaperreio.escalante_api.seguranca.model.application.ILoginService;
-import br.com.appsemaperreio.escalante_api.seguranca.model.application.mappers.PerfilMapper;
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class LoginService implements ILoginService {
@@ -38,30 +39,44 @@ public class LoginService implements ILoginService {
     }
 
     @Override
-    public String login(Authentication authentication, String perfilString) {
+    public String login(Authentication authentication, boolean loginAdmin) {
         
         var principal = (UserDetails) authentication.getPrincipal();
 
         var passwordsPadroes = Set.of(senhaInicial, senhaPadrao);
 
         for (String pass : passwordsPadroes) {
-            var invalido = pass == null || pass.isBlank();    
+            var invalido = pass == null || pass.isBlank();
             if (!invalido && passwordEncoder.matches(pass, principal.getPassword()))
                 return "";
         }
-        
-        var perfil = perfilMapper.stringToPerfil(perfilString);
 
-        var authority = authentication.getAuthorities().stream()
-                .filter(auth -> auth.getAuthority().equals("ROLE_" + perfil.name()))
-                .findFirst()
-                .orElseThrow(() -> new AccessDeniedException("Acesso negado para o perfil: " + perfil.name()));
+        var ehAdmin = authentication.getAuthorities().stream()
+                .anyMatch(auth -> auth.getAuthority().equals("ROLE_" + Perfil.ADMIN.name()));
+
+        if (!ehAdmin || authentication.getAuthorities().size() == 1) return jwtService.gerarToken(authentication);
+
+        if (loginAdmin) {
+            var authority = new SimpleGrantedAuthority("ROLE_" + Perfil.ADMIN.name());
+            var newAuthentication = new UsernamePasswordAuthenticationToken(
+                    authentication.getPrincipal(),
+                    null,
+                    Collections.singletonList(authority)
+            );
+
+            return jwtService.gerarToken(newAuthentication);
+        }
+
+        var authorities = authentication.getAuthorities().stream()
+                .filter(auth -> !auth.getAuthority().equals("ROLE_" + Perfil.ADMIN.name()))
+                .map(auth -> new SimpleGrantedAuthority(auth.getAuthority()))
+                .collect(Collectors.toSet());
 
         var newAuthentication = new UsernamePasswordAuthenticationToken(
                 authentication.getPrincipal(),
                 null,
-                Collections.singletonList(
-                        new SimpleGrantedAuthority(authority.getAuthority())));
+                authorities
+        );
 
         return jwtService.gerarToken(newAuthentication);
     }
