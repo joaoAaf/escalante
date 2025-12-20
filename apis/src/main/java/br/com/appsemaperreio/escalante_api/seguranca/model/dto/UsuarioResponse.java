84a9b43570d2b60dee2a0474ada9d84ba8bab3e6
João Anderson <joao.anderson.af@gmail.com>
2025-12-20 17:51:38 -0300

    Adicionando anotação JsonInclude à classe UsuarioResponse para omitir campos nulos

--- apis/src/main/java/br/com/appsemaperreio/escalante_api/seguranca/model/dto/UsuarioResponse.java
+++ apis/src/main/java/br/com/appsemaperreio/escalante_api/seguranca/model/dto/UsuarioResponse.java
@@ -1,9 +1,11 @@
 package br.com.appsemaperreio.escalante_api.seguranca.model.dto;
 
+import br.com.appsemaperreio.escalante_api.seguranca.model.domain.Perfil;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.Set;
 
-import br.com.appsemaperreio.escalante_api.seguranca.model.domain.Perfil;
-
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
 public record UsuarioResponse(String username, Set<Perfil> perfis, String senhaGerada) {
 
     public UsuarioResponse(String username, Set<Perfil> perfis) { this(username, perfis, null); }
