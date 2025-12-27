ef2a106c10aba8bbcca665d5c82390d7ec0d5fc6
João Anderson <joao.anderson.af@gmail.com>
2025-12-27 16:31:16 -0300

    Refatorando tabela de usuarios para popular com usuarios do banco de dados.

--- front/src/routes/usuarios/Usuarios.jsx
+++ front/src/routes/usuarios/Usuarios.jsx
@@ -1,13 +1,74 @@
 import Styles from './styles.module.css'
-import { useState } from 'react'
+import { useContext, useEffect, useRef, useState } from 'react'
 import BotaoRemover from '../../components/botao_remover/BotaoRemover'
+import { GlobalContext } from "../../context/GlobalContext.jsx";
+import UsuarioClient from "../../clients/UsuarioClient.js";
 
 export default function Usuarios() {
-    const [usuarios, setUsuarios] = useState([
-        { email: 'admin@example.com', perfil: 'ADMIN' },
-        { email: 'user1@example.com', perfil: 'USER' },
-        { email: 'user2@example.com', perfil: 'USER' }
-    ])
+
+    const { token, setFeedback } = useContext(GlobalContext)
+    const [usuarios, setUsuarios] = useState([])
+
+    const abortControllerRef = useRef(null)
+
+    useEffect(() => {
+        return () => {
+            if (abortControllerRef.current)
+                abortControllerRef.current.abort()
+        }
+    }, [])
+
+    const criarAbortController = () => {
+        abortControllerRef.current?.abort()
+        const controller = new AbortController()
+        abortControllerRef.current = controller
+        return controller
+    }
+
+    useEffect(() => {
+
+        const controller = criarAbortController()
+
+        UsuarioClient.listarUsuarios(token, controller.signal)
+            .then(usuarios => {
+                setUsuarios(usuarios || [])
+            })
+            .catch(error => {
+                if (error.name === 'AbortError') return
+                setFeedback({ type: 'error', mensagem: error.message })
+            })
+            .finally(() => {
+                if (abortControllerRef.current === controller)
+                    abortControllerRef.current = null
+            })
+    }, [])
+
+    const tabelaUsuarios = () => {
+        if (!usuarios || usuarios.length === 0) {
+            return (
+                <tr>
+                    <td colSpan={3}>Nenhum usuário encontrado.</td>
+                </tr>
+            )
+        }
+        return usuarios.map(usuario => {
+            return (
+                <tr key={usuario.username}>
+                    <td>{usuario.username}</td>
+                    <td>{usuario.perfis.join(' ')}</td>
+                    <td>
+                        <BotaoRemover
+                            tabela={usuarios}
+                            setTabela={setUsuarios}
+                            id={usuario.username}
+                            idKey={'username'}
+                            campos={["Email", "Perfil"]}
+                        />
+                    </td>
+                </tr>
+            )
+        })
+    }
 
     return (
         <div className={Styles.main}>
@@ -16,32 +77,18 @@ export default function Usuarios() {
             <div className={Styles.acoes}>
                 <button onClick={() => alert('Abrir modal de cadastro (exemplo)')}>Cadastrar Usuário</button>
             </div>
-                <table className={Styles.table}>
-                    <thead>
-                        <tr>
-                            <th>EMAIL</th>
-                            <th>PERFIL</th>
-                            <th>AÇÕES</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {usuarios.map((u, i) => (
-                            <tr key={i}>
-                                <td>{u.email}</td>
-                                <td>{u.perfil}</td>
-                                <td>
-                                    <BotaoRemover
-                                        tabela={usuarios}
-                                        setTabela={setUsuarios}
-                                        id={u.email}
-                                        idKey={'email'}
-                                        campos={["Email", "Perfil"]}
-                                    />
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+            <table className={Styles.table}>
+                <thead>
+                    <tr>
+                        <th>EMAIL</th>
+                        <th>PERFIL</th>
+                        <th>AÇÕES</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tabelaUsuarios()}
+                </tbody>
+            </table>
         </div>
     )
 }
