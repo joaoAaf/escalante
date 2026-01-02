c3e8933c21a05e344ca8277745b814c72cda154b
João Anderson <joao.anderson.af@gmail.com>
2026-01-02 13:49:48 -0300

    Adicionando funcionalidade de logout no perfil do usuário e ajustando estilos dos botões.

--- front/src/components/perfil_usuario/PerfilUsuario.jsx
+++ front/src/components/perfil_usuario/PerfilUsuario.jsx
@@ -1,6 +1,8 @@
 import Modal from '../modal/Modal.jsx'
 import Styles from './styles.module.css'
-import {useState} from "react";
+import {useContext, useState} from "react";
+import {useNavigate} from 'react-router-dom'
+import {GlobalContext} from '../../context/GlobalContext'
 import AtualizacaoEmail from "../atualizacao_email/AtualizacaoEmail.jsx";
 
 export default function PerfilUsuario({abrir, fechar, usuario, alterarSenha}) {
@@ -8,8 +10,16 @@ export default function PerfilUsuario({abrir, fechar, usuario, alterarSenha}) {
     const {username, perfis} = usuario || {username: "", perfis: []}
 
     const [email, setEmail] = useState(username)
+    const {setToken} = useContext(GlobalContext)
+    const navigate = useNavigate()
 
     if (!username || !perfis || perfis.length === 0) return
+
+    const logout = () => {
+        setToken("")
+        fechar()
+        navigate('/login')
+    }
 
     return (
         <Modal abrir={abrir} fechar={fechar} titulo="Meu Perfil">
@@ -20,9 +30,11 @@ export default function PerfilUsuario({abrir, fechar, usuario, alterarSenha}) {
             </span>
             <p>Perfis: <strong>{(perfis || []).join(', ')}</strong></p>
             <div className={Styles.acoes}>
-                <button className={Styles.alterarSenha} onClick={() => alterarSenha()}>Alterar senha</button>
+                <button id={Styles.alterar} className={Styles.botoes} onClick={() => alterarSenha()}>
+                    Alterar senha
+                </button>
+                <button id={Styles.sair} className={Styles.botoes} onClick={logout}>Sair</button>
             </div>
         </Modal>
     )
 }
-
