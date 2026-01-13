60a2d9d7b21819dff4be761410722ff0972ee922
João Anderson <joao.anderson.af@gmail.com>
2026-01-13 17:31:25 -0300

    Corrigindo colisões de antiguidade no cadastro e atualização de militares no postgresql.

--- apis/src/main/java/br/com/appsemaperreio/escalante_api/escalante/model/repository/MilitarRepository.java
+++ apis/src/main/java/br/com/appsemaperreio/escalante_api/escalante/model/repository/MilitarRepository.java
@@ -1,7 +1,9 @@
 package br.com.appsemaperreio.escalante_api.escalante.model.repository;
 
 import br.com.appsemaperreio.escalante_api.escalante.model.domain.Militar;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,9 +18,8 @@ public interface MilitarRepository extends JpaRepository<Militar, String> {
     @Query("SELECT m.id FROM Militar m WHERE m.id IN :matriculas")
     List<String> findMatriculasByIdIn(@Param("matriculas") Collection<String> matriculas);
 
-    @Modifying
-    @Query("UPDATE Militar m SET m.antiguidade = m.antiguidade + 1 WHERE m.antiguidade >= :antiguidade")
-    void increaseAntiguidadeFrom(@Param("antiguidade") Integer antiguidade);
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    List<Militar> findByAntiguidadeGreaterThanEqualOrderByAntiguidadeDesc(Integer antiguidade);
 
     @Query("SELECT COALESCE(MAX(m.antiguidade), 0) FROM Militar m")
     int maxAntiguidade();
